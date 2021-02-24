565d22fa1915fc4bd926f820cbe45596b0251b63
Ritik Kumar <ritik.kumar006@gmail.com>
2021-02-25 00:53:26 +0530

    Add home page colors

--- src/Components/Palette/PaletteList.js
+++ src/Components/Palette/PaletteList.js
@@ -1,16 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { withStyles } from "@material-ui/styles";
 
-const PaletteList = ({ palettes }) => {
+const styles = {
+  root: {
+    backgroundColor: "blue",
+    height: "100vh",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  container: {
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    color: "white",
+    justifyContent: "space-between",
+  },
+  palettes: {
+    width: "100%",
+    display: "grid",
+    boxSizing: "border-box",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%",
+  },
+};
+
+const PaletteList = ({ palettes, classes }) => {
   return (
-    <div>
-      <h1>React Colors</h1>
-      {palettes.map((palette, index) => (
-        <MiniPalette {...palettes[index]} />
-      ))}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+          {palettes.map((palette, index) => (
+            <MiniPalette {...palettes[index]} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
